3bdace57cb9ef768ce7a6589c70665e7b84c5810
Lei <lei@qq.com>
2025-08-13 19:38:10 +0800

    文章目录高亮

--- themes/hugo-tl/assets/ts/alpine-data/post.ts
+++ themes/hugo-tl/assets/ts/alpine-data/post.ts
@@ -14,23 +14,22 @@ const tocHighlight = () => ({
     createTocObserver() {
         const headings = document.querySelectorAll("article h1[id], article h2[id], article h3[id], article h4[id], article h5[id], article h6[id]")
         const setCurrentActive = () => {
-            const allActive = document.querySelectorAll(`#TableOfContents .active`)
+            const allActive = this.$el.querySelectorAll(`#TableOfContents .active`)
             if (allActive.length === 0) {
                 return
             } else {
-                document.querySelector(`#TableOfContents .current`)?.classList.remove('current');
-                document.querySelectorAll(`#TableOfContents .active`)[0]?.classList.add('current')
+                this.$el.querySelector(`#TableOfContents .current`)?.classList.remove('current');
+                this.$el.querySelectorAll(`#TableOfContents .active`)[0]?.classList.add('current')
             }
         }
 
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => {
                 const id = entry.target.getAttribute('id');
-                console.log(id,entry.intersectionRatio)
                 if (entry.intersectionRatio > 0) {
-                    document.querySelector(`#TableOfContents li a[href="#${id}"]`)?.parentElement?.classList.add('active');
+                    this.$el.querySelector(`#TableOfContents li a[href="#${id}"]`)?.parentElement?.classList.add('active');
                 } else {
-                    document.querySelector(`#TableOfContents li a[href="#${id}"]`)?.parentElement?.classList.remove('active');
+                    this.$el.querySelector(`#TableOfContents li a[href="#${id}"]`)?.parentElement?.classList.remove('active');
                 }
                 setCurrentActive()
             });
